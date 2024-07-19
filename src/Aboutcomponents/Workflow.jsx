74a4c0089957e79d0261a9f0d704e76c4fe39671
Ryaaha <vinmalikgithub@gmail.com>
2024-07-19 11:31:06 +0530

    Font fixes

--- src/Aboutcomponents/Workflow.jsx
+++ src/Aboutcomponents/Workflow.jsx
@@ -4,16 +4,15 @@ import { FaArrowRight } from "react-icons/fa";
 function Workflow()
 {
     return (
-        <div className="font-manrope mx-auto sm:px-4 px-4 sm:max-w-7xl w-screen sm:pt-28 sm:pb-12 pt-20 pb-20">
+        <div className="font-manrope mx-auto sm:px-4 px-4 sm:max-w-7xl w-screen sm:pt-24 pt-20 sm:pb-24 pb-16">
             {/* <div className="w-screen h-1/4 bg-bloo/10 rotate-45 absolute top-0 z-1 left-[50%] blur-[300px]"></div> */}
             {/* <div className="w-full h-full bg-bloo/5 -rotate-45 absolute right-[75%] blur-[400px]"></div> */}
             <div className="flex flex-col items-center justify-center text-center">
-                <h1 className="text-bloo font-bold sm:text-3xl text-2xl pb-2">Our Values</h1>
-                <h2 className="text-blackk font-bold sm:text-6xl text-4xl sm:leading-tight leading-snug">Delivering Reliable and Quality Software Development Services</h2>
+                <h1 className="text-bloo font-bold text-center text-xl sm:text-xl lg:text-2xl  py-2">Our Values</h1>
+                <h2 className="text-blackk  font-bold text-center text-2xl sm:text-2xl mx-auto md:text-3xl lg:text-[32px] max-w-3xl py-1">Delivering Reliable and Quality Software Development Services</h2>
             </div>
-            <div className="sm:py-16 py-12">
+            <div className="py-4">
                 <div className="flex flex-col items-center justify-center text-center text-blackk">
-                    <h1 className="pb-4 text-blackk/90 font-semibold sm:text-3xl text-2xl sm:leading-relaxed leading-snug">EICE's Transparent Workflow</h1>
                     <p className="font-medium text-blackk/60 sm:text-xl text-lg max-w-5xl">Ensuring clarity and visiblity throughout the development process to enhance collaboration and efficiency. Our workflow is well organized and flexible at its core.</p>
                 </div>
                 <div className="lg:flex lg:flex-row flex flex-col sm:pt-12 pt-12 items-center justify-center">
